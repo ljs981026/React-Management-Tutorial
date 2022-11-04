00bf1c248ea07add7f5dc92ace87d3b1e92254a3
ljs981026 <104576871+ljs981026@users.noreply.github.com>
2022-11-04 23:21:02 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생' 
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '이재석',
+    'birthday': '981026',
+    'gender': '남자',
+    'job': '개발자' 
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',  
+    'name': '심청이',
+    'birthday': '945021',
+    'gender': '여자',
+    'job': '회사원' 
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        customers.map(cust => {
+          return (
+            <Customer 
+              // map을 이용할때 구분할 수 있는 키값을 주어야한다.
+              key = {cust.id}
+              id = {cust.id}
+              image = {cust.image}
+              name = {cust.name}
+              birthday = {cust.birthday}
+              gender = {cust.gender}
+              job = {cust.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
