4c759e80fb624c86012428d182171f21a564ff74
ljs981026 <104576871+ljs981026@users.noreply.github.com>
2022-11-05 02:56:37 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
-import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { CircularProgress, Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import { render } from '@testing-library/react';
-import { withStyles } from '@mui/styles';
 import React, { Component } from 'react';
 
-const styles = theme => ({
-  root:{
-    width: '100%',
-    marginTop: "50",
-    overflowX: "auto"
-  },
-  table: {
-    minWidth: 1080
-  }
-})
+/*
+react lifecycle
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount()
+
+props or state가 변경되면 shouldComponentUpdate()를 불러와서 화면을 갱신해준다.
+*/
 
 class App extends Component {
   // 변경될 수 있는 데이터 처리 
   state = {
     customers: "",
+    completed: 0,
   }
 
   // api를 받아올때 사용
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -36,42 +39,51 @@ class App extends Component {
     return body;
   }
 
+  progress = () => {
+    const { completed } =this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
   render() {
-    const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {this.state.customers ? this.state.customers.map(cust => {
-            return(
-              <Customer 
-                // map을 이용할때 구분할 수 있는 키값을 주어야한다.
-                key = {cust.id}
-                id = {cust.id}
-                image = {cust.image}
-                name = {cust.name}
-                birthday = {cust.birthday}
-                gender = {cust.gender}
-                job = {cust.job}
-              />
-            )
-            }) : ""
-          }
-          </TableBody>
-        </Table>
-      </Paper>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {this.state.customers ? this.state.customers.map(cust => {
+              return(
+                <Customer 
+                  // map을 이용할때 구분할 수 있는 키값을 주어야한다.
+                  key = {cust.id}
+                  id = {cust.id}
+                  image = {cust.image}
+                  name = {cust.name}
+                  birthday = {cust.birthday}
+                  gender = {cust.gender}
+                  job = {cust.job}
+                />
+              )
+              }) : 
+              <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress variant="indeterminate" value={this.state.completed} />
+              </TableCell>
+              </TableRow>
+            }
+            </TableBody>
+          </Table>
+        </Paper>
     );
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
